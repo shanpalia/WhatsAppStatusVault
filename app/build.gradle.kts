8d0a61244b67f219c4d80d7c3160ad8214ece78b
shanpalia <shanpalia786@gmail.com>
2026-08-24 14:07:06 +0530

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,13 +63,6 @@ android {
 
 // Configure the Secrets Gradle Plugin to use .env and .env.example files
 // to match the convention used in Web projects.
-secrets {
-  propertiesFileName = ".env"
-  defaultPropertiesFileName = ".env.example"
-  ignoreList.add("FIREBASE_APPCHECK_DEBUG_TOKEN")
-}
-
-googleServices { missingGoogleServicesStrategy = MissingGoogleServicesStrategy.WARN }
 
 // Some unused dependencies are commented out below instead of being removed.
 // This makes it easy to add them back in the future if needed.
